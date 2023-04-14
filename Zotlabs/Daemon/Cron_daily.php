ad45205ddf53e41fe1ea42ae3ff8abe0d1846ced
Mario Vavti <mario@mariovavti.com>
2023-04-14 10:45:01 +0200

    this has been moved to cron for now

--- Zotlabs/Daemon/Cron_daily.php
+++ Zotlabs/Daemon/Cron_daily.php
@@ -94,17 +94,6 @@ class Cron_daily {
 		// expire any expired accounts
 		downgrade_accounts();
 
-		// If this is a directory server, request a sync with an upstream
-		// directory at least once a day, up to once every poll interval.
-		// Pull remote changes and push local changes.
-		// potential issue: how do we keep from creating an endless update loop?
-
-		$dirmode = get_config('system', 'directory_mode');
-
-		if ($dirmode == DIRECTORY_MODE_SECONDARY || $dirmode == DIRECTORY_MODE_PRIMARY) {
-			Libzotdir::sync_directories($dirmode);
-		}
-
 		Master::Summon(array('Expire'));
 		Master::Summon(array('Cli_suggest'));
 
