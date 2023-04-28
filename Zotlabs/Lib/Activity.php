354e341d69eb748a6ae1582fadade9d1012de7e4
Mario Vavti <mario@mariovavti.com>
2023-04-28 12:50:57 +0200

    mastodon says hashtag is a subset of link - issue #1750

--- Zotlabs/Lib/Activity.php
+++ Zotlabs/Lib/Activity.php
@@ -591,9 +591,9 @@ class Activity {
 			foreach ($item['term'] as $t) {
 				switch ($t['ttype']) {
 					case TERM_HASHTAG:
-						// id is required so if we don't have a url in the taxonomy, ignore it and keep going.
+						// href is required so if we don't have a url in the taxonomy, ignore it and keep going.
 						if ($t['url']) {
-							$ret[] = ['type' => 'Hashtag', 'id' => $t['url'], 'name' => '#' . $t['term']];
+							$ret[] = ['type' => 'Hashtag', 'href' => $t['url'], 'name' => '#' . $t['term']];
 						}
 						break;
 
