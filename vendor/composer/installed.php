51266138250d017951b588050860872ca42367a4
Mario <mario@mariovavti.com>
2023-06-02 16:36:57 +0000

    update bootstrap to version 5.3

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -269,9 +269,9 @@
             'dev_requirement' => false,
         ),
         'twbs/bootstrap' => array(
-            'pretty_version' => 'v5.3.0-alpha3',
-            'version' => '5.3.0.0-alpha3',
-            'reference' => 'ac576614a5515e429f27e756fad81d5aa05e95a6',
+            'pretty_version' => 'v5.3.0',
+            'version' => '5.3.0.0',
+            'reference' => '60098ac499d30aa50575b0b7137391c06ef25429',
             'type' => 'library',
             'install_path' => __DIR__ . '/../twbs/bootstrap',
             'aliases' => array(),
@@ -280,7 +280,7 @@
         'twitter/bootstrap' => array(
             'dev_requirement' => false,
             'replaced' => array(
-                0 => 'v5.3.0-alpha3',
+                0 => 'v5.3.0',
             ),
         ),
         'voku/portable-ascii' => array(
