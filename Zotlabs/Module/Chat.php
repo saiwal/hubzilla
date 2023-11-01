331622309f3f21ea08a1d767967c6143a16f49ef
Mario <mario@mariovavti.com>
2023-11-01 15:47:16 +0000

    if channel is not defined we need to provide an empty array

--- Zotlabs/Module/Chat.php
+++ Zotlabs/Module/Chat.php
@@ -225,7 +225,7 @@ class Chat extends Controller {
 
 		$o = '';
 
-		$acl = new AccessList($channel);
+		$acl = new AccessList($channel ?? []);
 		$channel_acl = $acl->get();
 
 		$lockstate = (($channel_acl['allow_cid'] || $channel_acl['allow_gid'] || $channel_acl['deny_cid'] || $channel_acl['deny_gid']) ? 'lock' : 'unlock');
