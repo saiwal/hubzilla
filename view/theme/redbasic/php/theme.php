547e6542ba7d396cc1c349577e2078fda098bee6
Mario <mario@mariovavti.com>
2023-11-07 10:09:31 +0000

    move color_mode related code to theme_init.php

--- view/theme/redbasic/php/theme.php
+++ view/theme/redbasic/php/theme.php
@@ -16,24 +16,4 @@
 
 function redbasic_init() {
 
-	$mode = '';
-	$navbar_mode = '';
-
-	if (local_channel()) {
-		$mode = ((get_pconfig(local_channel(), 'redbasic', 'dark_mode')) ? 'dark' : 'light');
-		$navbar_mode = ((get_pconfig(local_channel(), 'redbasic', 'navbar_dark_mode')) ? 'dark' : 'light');
-	}
-
-	if (App::$profile_uid) {
-		$mode = ((get_pconfig(App::$profile_uid, 'redbasic', 'dark_mode')) ? 'dark' : 'light');
-		$navbar_mode = ((get_pconfig(App::$profile_uid, 'redbasic', 'navbar_dark_mode')) ? 'dark' : 'light');
-	}
-
-	if (!$mode) {
-		$mode = ((get_config('redbasic', 'dark_mode')) ? 'dark' : 'light');
-		$navbar_mode = ((get_config('redbasic', 'navbar_dark_mode')) ? 'dark' : 'light');
-	}
-
-	App::$page['color_mode'] = 'data-bs-theme="' . $mode . '"';
-	App::$page['navbar_color_mode'] = (($navbar_mode === 'dark') ? 'data-bs-theme="' . $navbar_mode . '"' : '');
 }
--- view/theme/redbasic/php/theme_init.php
+++ view/theme/redbasic/php/theme_init.php
@@ -11,3 +11,24 @@ head_add_js('/vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js');
 head_add_js('/library/bootbox/bootbox.min.js');
 head_add_js('/library/bootstrap-tagsinput/bootstrap-tagsinput.js');
 head_add_js('/library/bootstrap-colorpicker/dist/js/bootstrap-colorpicker.js');
+
+$redbasic_mode = '';
+$redbasic_navbar_mode = '';
+
+if (local_channel()) {
+	$redbasic_mode = ((get_pconfig(local_channel(), 'redbasic', 'dark_mode')) ? 'dark' : 'light');
+	$redbasic_navbar_mode = ((get_pconfig(local_channel(), 'redbasic', 'navbar_dark_mode')) ? 'dark' : 'light');
+}
+
+if (App::$profile_uid) {
+	$redbasic_mode = ((get_pconfig(App::$profile_uid, 'redbasic', 'dark_mode')) ? 'dark' : 'light');
+	$redbasic_navbar_mode = ((get_pconfig(App::$profile_uid, 'redbasic', 'navbar_dark_mode')) ? 'dark' : 'light');
+}
+
+if (!$redbasic_mode) {
+	$redbasic_mode = ((get_config('redbasic', 'dark_mode')) ? 'dark' : 'light');
+	$redbasic_navbar_mode = ((get_config('redbasic', 'navbar_dark_mode')) ? 'dark' : 'light');
+}
+
+App::$page['color_mode'] = 'data-bs-theme="' . $redbasic_mode . '"';
+App::$page['navbar_color_mode'] = (($redbasic_navbar_mode === 'dark') ? 'data-bs-theme="' . $redbasic_navbar_mode . '"' : '');
